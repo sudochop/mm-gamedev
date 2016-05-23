a89f6541a50dbb829356013160c0f11ddb84d5d7
Michael Miller <michael.miller@dynamit.com>
2016-05-23 15:15:26 -0500

    Formatting

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,14 @@ int main(int argc, char* argv[]) {
 	bool running {true};
 
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        printf("SDL_Init\n");
-        throw std::runtime_error(SDL_GetError());
-    }
+		printf("SDL_Init\n");
+		throw std::runtime_error(SDL_GetError());
+	}
 
-    if (SDL_ShowCursor(SDL_DISABLE) < 0) {
-        printf("SDL_ShowCursor\n");
-        throw std::runtime_error(SDL_GetError());
-    }
+	if (SDL_ShowCursor(SDL_DISABLE) < 0) {
+		printf("SDL_ShowCursor\n");
+		throw std::runtime_error(SDL_GetError());
+	}
 
 	std::unique_ptr<SDL_Window, void(*)(SDL_Window*)> window(
 		SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN), 
