9a67ee16f0f95cc32a6c78bdc42b7c49f567f4a1
Michael Miller <michael.miller@dynamit.com>
2016-05-24 16:54:30 -0500

    Formatting

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,10 @@
 
 struct SDLDeleter {
 	void operator()(SDL_Window*		p) const { SDL_DestroyWindow(p); }
-	void operator()(SDL_Renderer* 	p) const { SDL_DestroyRenderer(p); }
-	void operator()(SDL_Texture* 	p) const { SDL_DestroyTexture(p); }
-	void operator()(SDL_Surface* 	p) const { SDL_FreeSurface(p); }
-	void operator()(TTF_Font* 		p) const { TTF_CloseFont(p); }
+	void operator()(SDL_Renderer*	p) const { SDL_DestroyRenderer(p); }
+	void operator()(SDL_Texture*	p) const { SDL_DestroyTexture(p); }
+	void operator()(SDL_Surface*	p) const { SDL_FreeSurface(p); }
+	void operator()(TTF_Font*		p) const { TTF_CloseFont(p); }
 };
 
 using SDLWindowPointer 		= std::unique_ptr<SDL_Window, 	SDLDeleter>;
