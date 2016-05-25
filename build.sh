cbf18735e8743eac668d1daa5749f28bf867795d
Michael Miller <michael.miller@dynamit.com>
2016-05-24 22:58:27 -0500

    Removed ttf link

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-g++ -Wall -Werror -Wpedantic -std=c++14 -g src/main.cpp -o bin/Game $(sdl2-config --cflags --libs) -lSDL_ttf
+g++ -Wall -Werror -Wpedantic -std=c++14 -g src/main.cpp -o bin/Game $(sdl2-config --cflags --libs)
