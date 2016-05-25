f0e240715725784124f0911864f8b3a6b4deb9d4
Michael Miller <michael.miller@dynamit.com>
2016-05-24 23:00:52 -0500

    Added links back in

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-g++ -Wall -Werror -Wpedantic -std=c++14 -g src/main.cpp -o bin/Game $(sdl2-config --cflags --libs)
+g++ -Wall -Werror -Wpedantic -std=c++14 -g src/main.cpp -o bin/Game -lSDL2 -lSDL2_ttf
